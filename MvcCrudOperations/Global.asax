1977236bbae66816cdd55b8c04ff6b10b32bc5fa
sk00538811 <39809608+sk00538811@users.noreply.github.com>
2018-08-19 18:48:26 +0530

    crud operations

---
+++ MvcCrudOperations/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcCrudOperations.MvcApplication" Language="C#" %>
---
+++ MvcCrudOperations/Models/StudentData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcCrudOperations.Models
+{
+    public class StudentData
+    {
+        public int intStudentId { get; set; }
+        public string strStudentName { get; set; }
+        public string strStudentDepartment { get; set; }
+
+    }
+}
